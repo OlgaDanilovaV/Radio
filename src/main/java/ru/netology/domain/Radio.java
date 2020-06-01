4b9d26f129c0f32c6c681506086bdf42de37a178
Kseniya <zlaya_kobra@bk.ru>
2020-06-01 15:48:33 +0300

    Added tests

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,10 +3,11 @@ package ru.netology.domain;
 public class Radio {
     private String name;
     private int maxRadioStation = 9;
-    private int minRadioStation;
-    private int currentRadioStation;
+    private int minRadioStation = 1;
+    private int currentRadioStation = 7;
     private int maxVolume = 10;
-    private int minVolume;
+    private int minVolume = 0;
+    private int currentSoundVolume = 5;
     private boolean on;
 
     public String getName() {
@@ -57,11 +58,58 @@ public class Radio {
         this.minVolume = minVolume;
     }
 
+    public int getCurrentSoundVolume() {
+        return currentSoundVolume;
+    }
+
+    public void setCurrentSoundVolume(int currentSoundVolume) {
+        this.currentSoundVolume = currentSoundVolume;
+    }
+
     public boolean isOn() {
         return on;
     }
 
     public void setOn(boolean on) {
         this.on = on;
+    }
+
+    public void nextRadioStation() {
+        if (currentRadioStation < maxRadioStation) {
+            currentRadioStation ++;
+        } else {
+            currentRadioStation = minRadioStation;
+        }
+    }
+
+    public void prevRadioStation() {
+        if (currentRadioStation > minRadioStation) {
+            currentRadioStation --;
+        } else {
+            currentRadioStation = maxRadioStation;
+        }
+    }
+
+    public void inputRadioStation() {
+        if (currentRadioStation > maxRadioStation) {
+            this.currentRadioStation = maxRadioStation;
+            return;
+        }
+        if (currentRadioStation < minRadioStation) {
+            this.currentRadioStation = minRadioStation;
+            return;
+        }
+    }
+
+    public void increaseSoundVolume() {
+        if (currentSoundVolume < maxVolume) {
+            currentSoundVolume ++;
+        }
+    }
+
+    public void decreaseSoundVolume() {
+        if (currentSoundVolume > minVolume) {
+            currentSoundVolume --;
+        }
     }
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,7 +1,57 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    @Test
+    public void nextRadioStation() {
+        Radio radio = new Radio();
+        radio.nextRadioStation();
+        int expected = 8;
+        int actual = radio.getCurrentRadioStation();
 
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prevRadioStation() {
+        Radio radio = new Radio();
+        radio.prevRadioStation();
+        int expected = 6;
+        int actual = radio.getCurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inputRadioStation() {
+        Radio radio = new Radio();
+        radio.inputRadioStation();
+        int expected = 7;
+        int actual = radio.getCurrentRadioStation();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void increaseSoundVolume() {
+        Radio radio = new Radio();
+        radio.increaseSoundVolume();
+        int expected = 6;
+        int actual = radio.getCurrentSoundVolume();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decreaseSoundVolume() {
+        Radio radio = new Radio();
+        radio.decreaseSoundVolume();
+        int expected = 4;
+        int actual = radio.getCurrentSoundVolume();
+
+        assertEquals(expected,actual);
+    }
 }
